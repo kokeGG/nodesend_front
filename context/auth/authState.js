3cb477d11ddc2744ed5af042969e7a8a60974770
kokeGG <kokehr99@gmail.com>
2022-11-11 22:03:21 -0600

    Context auth

---
+++ context/auth/authState.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useReducer } from "react";
+import authContext from "./authContext";
+import authReducer from "./authReducer";
+
+import {USUARIO_AUTENTICADO} from '../../types'
+
+const AuthState = ({ children }) => {
+  // Definir un state inicial
+  const initialState = {
+    token: "",
+    autenticado: null,
+    usuario: null,
+    mensaje: null,
+  };
+  // Definir el reducer
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  // Registrar nuevos usuarios
+  const registrarUsuario = datos => {
+    console.log(datos)
+  }
+
+  // Usuario autenticado
+  const usuarioAutenticado = nombre => {
+    dispatch({
+        type: USUARIO_AUTENTICADO,
+        payload: nombre
+    })
+  }
+
+  return (
+    <authContext.Provider
+      value={{
+        token: state.token,
+        autenticado: state.autenticado,
+        usuario: state.usuario,
+        mensaje: state.mensaje,
+        registrarUsuario,
+        usuarioAutenticado
+      }}
+    >
+      {children}
+    </authContext.Provider>
+  );
+};
+
+export default AuthState;
