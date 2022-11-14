510b1f080fc14a5bb779ae6892b10e26e139599a
kokeGG <kokehr99@gmail.com>
2022-11-14 11:13:55 -0600

    limpiando alertas

--- context/auth/authReducer.js
+++ context/auth/authReducer.js
@@ -1,13 +1,19 @@
-import {REGISTRO_EXITOSO} from '../../types'
+import { REGISTRO_EXITOSO, REGISTRO_ERROR, LIMPIAR_ALERTA } from "../../types";
 
 export default (state, action) => {
-    switch(action.type) {
-        case REGISTRO_EXITOSO:
-            return {
-                ...state,
-                mensaje: action.payload
-            }
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case REGISTRO_EXITOSO:
+    case REGISTRO_ERROR:
+      return {
+        ...state,
+        mensaje: action.payload,
+      };
+    case LIMPIAR_ALERTA:
+        return {
+            ...state,
+            mensaje: null
+        }
+    default:
+      return state;
+  }
+};
--- context/auth/authState.js
+++ context/auth/authState.js
@@ -3,7 +3,7 @@ import { useReducer } from "react";
 import authContext from "./authContext";
 import authReducer from "./authReducer";
 
-import {REGISTRO_EXITOSO} from '../../types'
+import { REGISTRO_EXITOSO, REGISTRO_ERROR, LIMPIAR_ALERTA } from "../../types";
 
 import clienteAxios from "../../config/axios";
 
@@ -19,27 +19,37 @@ const AuthState = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
 
   // Registrar nuevos usuarios
-  const registrarUsuario = async datos => {
+  const registrarUsuario = async (datos) => {
     try {
-      const respuesta = await clienteAxios.post('/api/usuarios', datos);
-      console.log(respuesta)
+      const respuesta = await clienteAxios.post("/api/usuarios", datos);
+      console.log(respuesta);
       console.log(respuesta.data.msg);
       dispatch({
         type: REGISTRO_EXITOSO,
-        payload: respuesta.data.msg
-      })
+        payload: respuesta.data.msg,
+      });
     } catch (error) {
-      console.log(error);
+      console.log(error.response.data.msg);
+      dispatch({
+        type: REGISTRO_ERROR,
+        payload: error.response.data.msg,
+      });
     }
-  }
+    // Limpiar la alerta después de 3 segundos.
+    setTimeout(() => {
+      dispatch({
+        type: LIMPIAR_ALERTA,
+      });
+    }, 3000);
+  };
 
   // Usuario autenticado
-  const usuarioAutenticado = nombre => {
+  const usuarioAutenticado = (nombre) => {
     dispatch({
-        type: USUARIO_AUTENTICADO,
-        payload: nombre
-    })
-  }
+      type: USUARIO_AUTENTICADO,
+      payload: nombre,
+    });
+  };
 
   return (
     <authContext.Provider
@@ -49,7 +59,7 @@ const AuthState = ({ children }) => {
         usuario: state.usuario,
         mensaje: state.mensaje,
         registrarUsuario,
-        usuarioAutenticado
+        usuarioAutenticado,
       }}
     >
       {children}
